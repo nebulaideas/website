36555ef3008bea5088e10b082f2e22e74f8ef8b9
Ismael G Marin C <ismael.marin@gmail.com>
2026-06-03 02:11:20 -0600

    fix lint

--- app/src/hooks/useLanguage.tsx
+++ app/src/hooks/useLanguage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState, useCallback, useEffect, useMemo, type ReactNode } from 'react';
 import { translations, type Language, type TranslationKey } from '@/lib/translations';
 import { updateMetaTags } from '@/lib/meta';
